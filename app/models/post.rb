c6dee75014b6ee8617c956790a01dd580e270097
Tufahel <tufahel96@gmail.com>
2022-08-09 16:58:47 +0600

    post counter and 5 most recent post method added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

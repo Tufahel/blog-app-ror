88b897ddab3ac58c52e83ed2dc54a6e1c4689ee7
Tufahel <tufahel96@gmail.com>
2022-08-15 19:59:28 +0600

    controller updated for required 4 urls

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,6 @@
 class PostsController < ApplicationController
   def index; end
 
-  def new; end
-
   def show; end
 
-  def create; end
-
-  def destroy; end
-
-  def post_params; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,5 @@
 class UsersController < ApplicationController
   def index; end
 
-  def new; end
-
   def show; end
-
-  def create; end
-
-  def destroy; end
-
-  def user_params; end
 end

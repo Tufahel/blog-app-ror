59b8f83e6805242e938b19e2fdce9b0367779a8d
Tufahel <tufahel96@gmail.com>
2022-08-19 03:14:20 +0600

    fix bug to add data in database

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,6 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
-
   def update_posts_counter
     author.increment!(:posts_counter)
   end

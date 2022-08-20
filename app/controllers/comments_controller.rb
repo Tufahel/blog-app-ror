6ee7b1e32ae80156193cfb5c135705c1e8ada40a
Tufahel <tufahel96@gmail.com>
2022-08-20 13:36:55 +0600

    add new comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,23 @@
+class CommentsController < ApplicationController
+    def new
+        @comment = Comment.new
+    end
+
+    def create
+        @comment = Comment.new(comment_params)
+        @post = Post.find(params[:post_id])
+        @comment.author_id = current_user.id
+        @comment.post_id = @post.id
+
+        if @comment.save
+            redirect_to user_post_path(author_id: @post.author_id, id: @post.id)
+        else
+            render :new, alert: 'An error occured'
+        end
+    end
+
+    def comment_params
+        params.require(:comment).permit(:text)
+    end
+
+end

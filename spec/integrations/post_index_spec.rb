33bcfcc4e310b6089b92835e3a2968733566d4c7
Tufahel <tufahel96@gmail.com>
2022-08-22 19:37:34 +0600

    fix post_index_spec

--- spec/integrations/post_index_spec.rb
+++ spec/integrations/post_index_spec.rb
@@ -1,38 +1,50 @@
+require 'rails_helper'
 RSpec.describe 'Posts show page', type: :feature do
   before(:each) do
     User.destroy_all
-    @user = User.create(name: 'Tufahel', photo: 'pic.png', bio: 'Developer from Bangladesh',
-                        email: 'tufahel96@mail.com', password: '1234abc',
-                        confirmed_at: Time.now, role: 'admin', posts_counter: 0)
+    @user = User.create(name: 'Tufahel', photo: 'pic.png', bio: 'Developer from Bangladesh', 
+                        posts_counter: 0)
     Post.create(title: 'Title', text: 'Text', author_id: @user.id,
-                likes_counter: 0, comments_counter: 0)
+                likes_counter: 0, comments_counter: 0, id: 2)
     @comment = Comment.create(text: 'First comment', author: User.first, post: Post.first)
     @comment = Comment.create(text: 'Second comment', author: User.first, post: Post.first)
     @like = Like.create(author_id: User.first.id, post_id: Post.first.id)
+    visit user_posts_path(@user.id)
+    visit "/users/#{@user.id}/posts"
   end
   describe 'View posts' do
-    scenario 'See a section of pagination' do
-      expect(page).to have_content('1')
-      expect(page).to have_content('2')
+    scenario 'I can see the users profile picture.' do
+      expect(page.first('img')['src']).to have_content 'pic.png'
+    end
+    scenario 'I can see the users username.' do
+      expect(page).to have_content 'Tufahel'
+    end
+    scenario 'I can see the number of posts the user has written.' do
+      expect(page).to have_content 'Number of posts: 1'
     end
     scenario 'I can see the posts title.' do
         expect(page).to have_content 'Title'
     end
-    scenario 'I can see who wrote the post' do
-      expect(page).to have_content 'Tufahel'
-    end
-    scenario 'I can see the post text.' do
+    scenario 'I can see the posts body.' do
       expect(page).to have_content 'Text'
     end
-    scenario 'I can see the post likes counter.' do
+    scenario 'I can see the first comments on a post.' do
+      expect(page).to have_content 'First comment'
+      expect(page).to have_content 'Second comment'
+    end
+    scenario 'I can see how many comments a post has.' do
+      expect(page).to have_content 'Comments: 2'
+    end
+    scenario 'I can see how many likes a post has.' do
       expect(page).to have_content 'Likes: 1'
     end
-    scenario 'I can see the post comments.' do
-        expect(page).to have_content 'First comment'
-        expect(page).to have_content 'Second comment'
+    scenario 'See a section of pagination' do
+      expect(page).to have_content('1')
+      expect(page).to have_content('2')
     end
-    scenario 'I can see the post comments counter.' do
-        expect(page).to have_content 'Comments: 2'
+    scenario "When I click on a post, it redirects me to that post's show page." do
+      visit user_post_path(@user.id, '2')
+      expect(page).to have_current_path user_post_path(@user.id, '2')
     end
   end
 end

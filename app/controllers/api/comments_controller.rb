050ec70eac6a34bcb221fcf6fd49db27dff8d2ea
David Abol Ger <draker74@gmail.com>
2022-08-27 11:37:37 +0300

    creating a json api folder, adding api to routes

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,32 @@
+class Api::CommentsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:post_id])
+    @comments = @post.comments
+    render json: @comments
+
+  end
+  def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find(params[:post_id])
+    @comment.author_id = current_user.id
+    @comment.post_id = @post.id
+
+    if @comment.save
+      redirect_to user_post_path(author_id: @post.author_id, id: @post.id)
+      render json: @comment
+    else
+      render :new, alert: 'An error occured'
+    end
+
+
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+
+end
---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::UsersController < ApplicationController
+  def index
+    @all_users = User.all
+
+    render json: @all_users
+
+  end
+  def show
+    @user = User.find(params[:id])
+
+    render json: @user
+  end
+
+end

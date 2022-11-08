87ee980723f11e82c09f3e0c862de15fae2015c4
Tufahel <tufahel96@gmail.com>
2022-11-08 14:59:55 +0600

    cors updated

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins 'http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'
-    origins '*'
+    origins ["https://euphonious-semolina-b80229.netlify.app", "http://euphonious-semolina-b80229.netlify.app", "http://localhost:3000", "http://localhost:3001"]
 
     resource '*',
              headers: :any,
-             methods: %i[get post put patch delete options head]
+             methods: %i[get post put patch delete options head],
+             expose: ["Authorization", "access-token", "expiry", "client", "uid", "token-type"],
+             max_age: 600
   end
 end
